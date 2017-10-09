0c80cc566086c944fb3faaa6e28fb2046178c825
andrewrgoss <andrewrgoss@gmail.com>
2017-10-09 14:03:28 -0400

    completed 4.16 lecture on variables, datatypes, and operators

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,17 @@
 
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
