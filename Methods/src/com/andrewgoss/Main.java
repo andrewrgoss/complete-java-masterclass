ac75ecede691b3874f072b082bb2c6a62ca49663
andrewrgoss <andrewrgoss@gmail.com>
2017-11-17 16:46:26 -0500

    initial commit

---
+++ Methods/src/com/andrewgoss/Main.java
@@ -0,0 +1,26 @@
+package com.andrewgoss;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        calculateScore(true, 800, 5, 100);
+
+        calculateScore(true, 10000, 8, 200);
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        }
+
+        return -1;
+
+    }
+
+}
