be03a4c15a1a220066ed77d7a68863108ea0e633
andrewrgoss <andrewrgoss@gmail.com>
2017-12-10 22:04:03 -0500

    initial commit

---
+++ Inheritance/src/com/andrewgoss/Dog.java
@@ -0,0 +1,51 @@
+package com.andrewgoss;
+
+public class Dog extends Animal { // inherit from another class using 'extends' keyword
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight); // super class means to call the constructor from the class you are extending (inheriting) from
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override // generate -> override method(s). This method overrides eat() method  from superclass (Animal class)
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat(); // super class for current class is animal class
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        super.move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(10);
+    }
+
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called");
+        moveLegs(speed);
+        super.move(speed);
+    }
+}
