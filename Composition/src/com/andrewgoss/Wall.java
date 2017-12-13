f80a82b9f3b6c0a440165ba10018d20c7b003068
andrewrgoss <andrewrgoss@gmail.com>
2017-12-13 12:05:18 -0500

    initial commit

---
+++ Composition/src/com/andrewgoss/Wall.java
@@ -0,0 +1,14 @@
+package com.andrewgoss;
+
+public class Wall {
+    private String direction;
+
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
