a4baf84ed1eec05d370fc8a369571f0d34af9b45
andrewrgoss <andrewrgoss@gmail.com>
2017-12-13 12:06:01 -0500

    refactored per course challenge solution

--- Composition/src/com/andrewgoss/Main.java
+++ Composition/src/com/andrewgoss/Main.java
@@ -25,7 +25,21 @@ public class Main {
         // but to access the object used in composition within the main class
         // like you saw in this video.
 
-        House house = new House(4, 10, 1, new Room(1, 2, 2));
-        house.setUpRoom();
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Andrews", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        bedroom.makeBed();
+
+        bedroom.getLamp().turnOn();
+
     }
 }
