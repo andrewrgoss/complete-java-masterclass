82a0143f8f13dc913f958678818fda64aeb7c70a
andrewrgoss <andrewrgoss@gmail.com>
2018-01-02 15:02:36 -0500

    initial commit

---
+++ ListAndArrayList/src/com/andrewgoss/GroceryList.java
@@ -0,0 +1,58 @@
+package com.andrewgoss;
+
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGoceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        for(int i = 0; i < groceryList.size(); i++) {
+            System.out.println((i + 1) + ". " + groceryList.get(i));
+        }
+    }
+
+    public void modifyGroceryItem(String currentItem, String newItem) {
+        int position = findItem(currentItem);
+        if(position >= 0) {
+            modifyGroceryItem(position, newItem);
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position + 1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(String item) {
+        int position = findItem(item);
+        if(position >= 0) {
+            removeGroceryItem(position);
+        }
+    }
+
+    public void removeGroceryItem(int position) {
+        groceryList.remove(position);
+    }
+
+    private int findItem(String searchItem) {
+        return groceryList.indexOf(searchItem);
+    }
+
+    public boolean onFile(String searchItem) {
+        int position = findItem(searchItem);
+        if(position >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
