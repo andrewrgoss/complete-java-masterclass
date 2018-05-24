8f3f3f3be8e597932f78bfd18bde3e9f765cd79e
andrewrgoss <andrewrgoss@gmail.com>
2018-05-24 16:29:12 -0400

    directory duplicate fix

---
+++ Sec5 - Expressions, Statements, Code blocks, Methods/StatementsWhiteSpaceAndIndenting/src/com/andrewgoss/Main.java
@@ -0,0 +1,17 @@
+package com.andrewgoss;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50; // entire line is a Java statement
+        myVariable ++;
+        myVariable --;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+        " another " +
+        "still more.");
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+    }
+}
