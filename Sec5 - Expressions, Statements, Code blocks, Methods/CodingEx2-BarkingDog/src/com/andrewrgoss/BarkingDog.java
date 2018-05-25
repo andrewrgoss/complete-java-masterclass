6b14f64dff784958b273010d40eab456e71ce2fe
andrewrgoss <andrewrgoss@gmail.com>
2018-05-25 16:53:09 -0400

    refactored to use less lines of code

--- Sec5 - Expressions, Statements, Code blocks, Methods/CodingEx2-BarkingDog/src/com/andrewrgoss/BarkingDog.java
+++ Sec5 - Expressions, Statements, Code blocks, Methods/CodingEx2-BarkingDog/src/com/andrewrgoss/BarkingDog.java
@@ -3,17 +3,9 @@ package com.andrewrgoss;
 public class BarkingDog {
 
     public static boolean bark(boolean barking, int hourOfDay) {
-        if (hourOfDay < 0 || hourOfDay > 23) {
-            System.out.println("Invalid hour of the day provided!");
-            return false;
-        } else if (!barking) {
-            System.out.println("The dog is not barking!");
-            return false;
-        } else if (hourOfDay < 8 || hourOfDay > 22) {
-            System.out.println("Dog is barking, wake up!");
-            return true;
-        } else {
+        if(hourOfDay < 0 || hourOfDay > 23) {
             return false;
         }
+        return barking && (hourOfDay < 8 || hourOfDay > 22);
     }
 }
--- Sec5 - Expressions, Statements, Code blocks, Methods/CodingEx2-BarkingDog/src/com/andrewrgoss/Main.java
+++ Sec5 - Expressions, Statements, Code blocks, Methods/CodingEx2-BarkingDog/src/com/andrewrgoss/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     // https://www.udemy.com/java-the-complete-java-developer-course/learn/v4/t/quiz/4426608
     public static void main(String[] args) {
-        System.out.println(BarkingDog.bark(true, -1));
+        System.out.println(BarkingDog.bark(true, 8));
     }
 }
